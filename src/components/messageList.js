c08dca5954e4442b9ace608283c28a4ffc721cbf
Justin Lui <justincylui@gmail.com>
2020-04-19 10:50:30 -0700

    queueing added....just need to clean it up

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -10,6 +10,10 @@ import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import MessageColumn from './messageColumn'
+import SnackbarContent from '@material-ui/core/SnackbarContent'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
+import Snackbar from '@material-ui/core/Snackbar'
 
 const MessageList = () => {
   const initializeState = () =>
@@ -21,9 +25,47 @@ const MessageList = () => {
   const [state, setState] = useState(initializeState())
   const restartStateRef = useRef(null)
 
+  const queueRef = React.useRef([])
+  const [open, setOpen] = React.useState(false)
+  const [messageInfo, setMessageInfo] = React.useState(undefined)
+
+  const processQueue = () => {
+    if (queueRef.current.length > 0) {
+      setMessageInfo(queueRef.current.shift())
+      setOpen(true)
+    }
+  }
+
+  const enqueue = message => {
+    queueRef.current.push(message)
+
+    if (open) {
+      // immediately begin dismissing current message
+      // to start showing new one
+      setOpen(false)
+    } else {
+      processQueue()
+    }
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpen(false)
+  }
+
+  const handleExited = () => {
+    processQueue()
+  }
+
   const handleMessageCallback = newMsg => {
     const priority = priorityMap[newMsg.priority]
     const newState = restartStateRef.current || state
+
+    if (priority === 'error') {
+      enqueue(newMsg)
+    }
 
     setState({
       ...newState,
@@ -42,7 +84,8 @@ const MessageList = () => {
 
   const deleteMessage = (id, priority) => {
     const messageIndex = state[priority].findIndex(data => data.id === id)
-    const updatedList = state[priority].splice(messageIndex, 1) && state[priority]
+    const updatedList =
+      state[priority].splice(messageIndex, 1) && state[priority]
     setState({ ...state, [priority]: updatedList })
   }
 
@@ -79,6 +122,37 @@ const MessageList = () => {
       </Grid>
 
       <pre>state {JSON.stringify(state, undefined, 2)}</pre>
+
+      <Snackbar
+        key={messageInfo ? messageInfo.key : undefined}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        onExited={handleExited}
+      >
+        <SnackbarContent
+          style={{
+            backgroundColor: priorityColorsMap['error']
+          }}
+          message={<span id="client-snackbar">{messageInfo && messageInfo.message}</span>}
+          action={
+            <>
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+              >
+                <CloseIcon />
+              </IconButton>
+            </>
+          }
+        />
+      </Snackbar>
+
       <ConsecutiveSnackbars />
     </Grid>
   )
