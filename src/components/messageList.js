fcdd9b531a4002e4b13ad94962dec409cece7815
Justin Lui <justincylui@gmail.com>
2020-04-18 17:59:46 -0700

    clear messages added

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 import Button from '@material-ui/core/Button'
 import useApi from '../hooks/useApi'
@@ -13,13 +13,21 @@ const MessageList = () => {
     }, {})
 
   const [state, setState] = useState(initializeState())
+  const restartStateRef = useRef(null)
 
   const handleMessageCallback = newMsg => {
     const priority = priorityMap[newMsg.priority]
+    const newState = restartStateRef.current || state
+    
     setState({
-      ...state,
-      [priority]: state[[priority]].push(newMsg) && state[[priority]]
+      ...newState,
+      [priority]: newState[[priority]].push({...newMsg, id: new Date().toISOString()}) && newState[[priority]]
     })
+  }
+
+  const clearMessages = () => {
+    setState(initializeState())
+    restartStateRef.current = initializeState()
   }
 
   const [apiStarted, toggleApiStarted] = useApi(handleMessageCallback)
@@ -29,6 +37,10 @@ const MessageList = () => {
       <Button variant="contained" onClick={() => toggleApiStarted(!apiStarted)}>
         {apiStarted ? 'Stop Messages' : 'Start Messages'}
       </Button>
+      <Button variant="contained" onClick={clearMessages}>
+        Clear
+      </Button>
+
       <pre>state {JSON.stringify(state, undefined, 2)}</pre>
       <ConsecutiveSnackbars />
     </>
