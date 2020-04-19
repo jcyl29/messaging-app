4ff9a7d64bab0eaa0e3291fc9c316645a5a26e21
Justin Lui <justincylui@gmail.com>
2020-04-18 17:13:47 -0700

    latest progress

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -6,11 +6,20 @@ import ConsecutiveSnackbars from './demo'
 import priorityMap from '../enums'
 
 const MessageList = () => {
-  const [messages, setMessages] = useState([])
+  const initializeState = () =>
+    Object.values(priorityMap).reduce((acc, value) => {
+      acc[value] = []
+      return acc
+    }, {})
+
+  const [state, setState] = useState(initializeState())
 
   const handleMessageCallback = newMsg => {
-    const result = { ...newMsg, priority: priorityMap[newMsg.priority] }
-    setMessages([...(messages.push(result) && messages)])
+    const priority = priorityMap[newMsg.priority]
+    setState({
+      ...state,
+      [priority]: state[[priority]].push(newMsg) && state[[priority]]
+    })
   }
 
   const [apiStarted, toggleApiStarted] = useApi(handleMessageCallback)
@@ -20,7 +29,7 @@ const MessageList = () => {
       <Button variant="contained" onClick={() => toggleApiStarted(!apiStarted)}>
         {apiStarted ? 'Stop Messages' : 'Start Messages'}
       </Button>
-      <pre>state {JSON.stringify(messages, undefined, 2)}</pre>
+      <pre>state {JSON.stringify(state, undefined, 2)}</pre>
       <ConsecutiveSnackbars />
     </>
   )
